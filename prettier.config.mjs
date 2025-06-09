e697673c27cf88ebdd73f34958d04d3ad6dedff1
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2025-06-09 18:44:32 +0300

    config: set up project

---
+++ prettier.config.mjs
@@ -0,0 +1,16 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 100,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  trailingComma: 'none',
+  plugins: ['prettier-plugin-tailwindcss']
+}
+
+export default config
