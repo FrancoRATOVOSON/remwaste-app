d6b831b7540eaeb45af1df20d607802e1247e1fd
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2025-06-11 14:30:51 +0300

    Update stepper.tsx

--- src/components/common/stepper/stepper.tsx
+++ src/components/common/stepper/stepper.tsx
@@ -1,6 +1,6 @@
 import { cn } from '@/lib/utils'
 import { Check, type LucideIcon } from 'lucide-react'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 export interface Step {
   label: string
@@ -13,13 +13,25 @@ interface StepperProps {
 }
 
 export function Stepper({ steps, currentStep }: StepperProps) {
+  const currentStepRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (currentStepRef.current)
+      currentStepRef.current.scrollIntoView({
+        behavior: 'smooth',
+        inline: 'center',
+        block: 'center'
+      })
+  }, [])
+
   return (
-    <div className="flex w-full items-center">
+    <div className="flex w-full items-center overflow-auto">
       {steps.map((step, index) => (
         <React.Fragment key={index}>
           <div
+            ref={index === currentStep ? currentStepRef : null}
             className={cn(
-              'flex items-center gap-2 rounded-xl px-1 py-1',
+              'flex shrink-0 items-center gap-2 rounded-xl px-1 py-1',
               index < currentStep && 'bg-primary/20 text-primary border-primary border',
               index === currentStep && 'bg-primary text-primary-foreground px-2.5 py-2.5'
             )}
@@ -42,7 +54,7 @@ export function Stepper({ steps, currentStep }: StepperProps) {
             <div className="text-center text-xs">{step.label}</div>
           </div>
 
-          {index < steps.length - 1 && <div className="mx-2 h-0.5 flex-1 bg-black" />}
+          {index < steps.length - 1 && <div className="mx-2 h-0.5 min-w-2 flex-1 bg-black" />}
         </React.Fragment>
       ))}
     </div>
